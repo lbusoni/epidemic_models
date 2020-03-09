243f933489986a4af59aa25d19c6986f92f65d73
lbusoni <lbusoni@gmail.com>
2020-03-09 12:37:45 +0100

    fix integration (should use RK but I'm lazy)

---
+++ epidemic_models/mains/main200309.py
@@ -0,0 +1,112 @@
+'''
+Created on Mar 6, 2020
+
+@author: lbusoni
+'''
+from epidemic_models import simple_sir, restore_data
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import convolve
+
+
+def main():
+    delayI = 36
+    delayR = 34
+    delayH = 0
+    strdates = 'Days (Hubei from Jan1, Italy from Feb6)'
+
+    de = restore_data.restoreCSSEDeath()
+    deH = de['Hubei/Mainland China'].values
+    deHd = de['Hubei/Mainland China'].days - delayH
+    deI = de['Italy'].values
+    deId = de['Italy'].days - delayI
+    deR = de['Iran'].values
+    deRd = de['Iran'].days - delayR
+
+    co = restore_data.restoreCSSEConfirmed()
+    coH = co['Hubei/Mainland China'].values
+    coHd = co['Hubei/Mainland China'].days - delayH
+    coI = co['Italy'].values
+    coId = co['Italy'].days - delayI
+    coR = co['Iran'].values
+    coRd = co['Iran'].days - delayR
+
+    plt.figure()
+    plt.plot(deId, deI, '.-', label='Deaths Italy')
+    plt.plot(deRd, deR, '.-', label='Deaths Iran')
+    plt.plot(deHd, deH, '.-', label='Deaths Hubei')
+    t = "Evolution of deaths in Hubei, Italy, Iran"
+    plt.title(t)
+    plt.legend()
+    plt.grid(True)
+    plt.ylabel('Total deaths')
+    plt.xlabel(strdates)
+    plt.show()
+    plt.semilogy()
+
+    co2de = 0.04
+    delay_co2de = 2
+    ff = np.ones(int(delay_co2de)) / delay_co2de
+    expDeH = co2de * convolve(coH, ff, mode='full')[:len(coH)]
+    expDeI = co2de * convolve(coI, ff, mode='full')[:len(coI)]
+    expDeR = co2de * convolve(coR, ff, mode='full')[:len(coR)]
+    plt.figure()
+    plt.plot(deId, deI, '.-', label='Deaths Italy')
+    plt.plot(deRd, deR, '.-', label='Deaths Iran')
+    plt.plot(deHd, deH, '.-', label='Deaths Hubei')
+    plt.plot(coId, coI, '.-', label='Confirmed Italy')
+    plt.plot(coRd, coR, '.-', label='Confirmed Iran')
+    plt.plot(coHd, coH, '.-', label='Confirmed Hubei')
+    plt.plot(coId, expDeI, label='Expected Deaths Italy')
+    plt.plot(coRd, expDeR, label='Expected Deaths Iran')
+    plt.plot(coHd, expDeH, label='Expected Deaths Hubei')
+    t = "Confirmed 2 Dead\n co2de=%g, delay=%g d" % (co2de, delay_co2de)
+    plt.title(t)
+    plt.legend()
+    plt.grid(True)
+    plt.ylabel('Total deaths')
+    plt.xlabel(strdates)
+    plt.ylim(1,)
+    plt.show()
+    plt.semilogy()
+
+    pop = 6e7
+    nSteps = 100
+    delayModel = 0
+    beta = np.zeros(nSteps)
+    beta[:30] = 0.6
+    beta[30:] = 0.3
+    gamma = 0.3
+    system = simple_sir.SimpleSIR(pop, 1, 0, beta, 1 / gamma, nSteps)
+    system.evolveSystem()
+    system.plot(susceptibles=False, infectives=False, recovered=False)
+
+    beta2 = np.zeros(nSteps)
+    beta2[:37] = 0.6
+    beta2[37:] = 0.3
+    gamma2 = 0.3
+    system2 = simple_sir.SimpleSIR(pop, 1, 0, beta2, 1 / gamma2, nSteps)
+    system2.evolveSystem()
+
+    beta3 = 0.6
+    gamma3 = 0.3
+    system3 = simple_sir.SimpleSIR(pop, 1, 0, beta3, 1 / gamma3, nSteps)
+    system3.evolveSystem()
+
+    cfr = 0.03
+    deaths = cfr * system.timeSeries.recoveredWithImmunity
+    plt.plot(deaths, label='deaths if R0=0 at day=30')
+    deaths2 = cfr * system2.timeSeries.recoveredWithImmunity
+    plt.plot(deaths2, label='deaths if R0=0 at day=37')
+    deaths3 = cfr * system3.timeSeries.recoveredWithImmunity
+    plt.plot(deaths3, label='deaths no action')
+
+    plt.plot(deHd + delayModel, deH, '.-', label='deaths Hubei')
+    plt.plot(deId + delayModel, deI, '.-', label='deaths Italy')
+    # plt.plot(coHd + delayModel, coH, '.-', label='confirmed Hubei')
+    # plt.plot(coId + delayModel, coI, '.-', label='confirmed Italy')
+    plt.ylim(10,)
+    plt.xlabel(strdates)
+    plt.legend()
+
--- epidemic_models/simple_sir.py
+++ epidemic_models/simple_sir.py
@@ -86,6 +86,7 @@ class SimpleSIR(object):
             self._gamma = 1. / average_infection_period
 
         self._dt = 1
+        self._nSubSteps = 10
 
         self._timeSeries = PopulationTimeSeries()
 
@@ -93,52 +94,85 @@ class SimpleSIR(object):
     def timeSeries(self):
         return self._timeSeries
 
+    @property
     def currentPopulation(self):
         return self._population
 
+    @property
     def contactRate(self):
         return self._beta
 
+    @property
     def averageInfectiousPeriod(self):
         return 1. / self._gamma
 
+    @property
     def totalPopulation(self):
         return self._N
 
+    @property
     def basicReproductionNumber(self):
         return self._beta / self._gamma
 
+    @property
     def forceOfInfection(self):
         return self._beta * self._timeSeries.infectives / \
-                self._timeSeries.totalPopulation
+            self._timeSeries.totalPopulation
+
+    @property
+    def exponentialTimeConstant(self):
+        return 1 / (self._beta - self._gamma)
+
+    @property
+    def delayRecoveredInfected(self):
+        return -np.log(self.basicReproductionNumber - 1) * \
+            self.exponentialTimeConstant
+
+    def timeToIncreaseByFactor(self, factor):
+        return self.exponentialTimeConstant * np.log(factor)
 
     def _singleStep(self, step):
-        cp = self.currentPopulation()
-        dt = self._dt
-        dS = -self._beta[step] * cp.susceptibles * cp.infectives / cp.totalPopulation
-        dI = -dS - self._gamma[step] * cp.infectives
-        dR = self._gamma[step] * cp.infectives
-        S = np.clip(cp.susceptibles + dS * dt, 0, cp.totalPopulation)
-        self._population = Population(S,
-                                     cp.infectives + dI * dt,
-                                     cp.recoveredWithImmunity + dR * dt)
+        cp = self.currentPopulation
+        Su = cp.susceptibles
+        In = cp.infectives
+        Re = cp.recoveredWithImmunity
+        Nu = cp.totalPopulation
+        dt = self._dt / self._nSubSteps
+        for _ in np.arange(self._nSubSteps):
+            dS = -self._beta[step] * Su * In / Nu
+            dI = -dS - self._gamma[step] * In
+            dR = self._gamma[step] * In
+            Su = Su + dS * dt
+            In = In + dI * dt
+            Re = Re + dR * dt
+        self._population = Population(np.clip(Su, 0, Nu),
+                                      In, Re)
 
     def evolveSystem(self):
         for i in np.arange(self._nSteps):
-            self._timeSeries.append(self.currentPopulation(), self._dt * i)
+            self._timeSeries.append(self.currentPopulation, self._dt * i)
             self._singleStep(i)
 
-    def plot(self):
+    def plot(self, susceptibles=True, infectives=True, recovered=True,
+             newFigure=True):
+
         ts = self._timeSeries
-        plt.figure()
-        plt.plot(ts.timeVector, ts.susceptibles, label='Susceptibles')
-        plt.plot(ts.timeVector, ts.infectives, label='Infectives')
-        plt.plot(ts.timeVector, ts.recoveredWithImmunity, label='Recovered with Immunity')
+        if newFigure:
+            plt.figure()
+        if susceptibles:
+            plt.plot(ts.timeVector, ts.susceptibles, label='Susceptibles')
+        if infectives:
+            plt.plot(ts.timeVector, ts.infectives, label='Infectives')
+        if recovered:
+            plt.plot(ts.timeVector,
+                     ts.recoveredWithImmunity,
+                     label='Recovered with Immunity')
         # plt.plot(ts.timeVector, ts.justInfected, label='Just Infected')
         # plt.plot(ts.timeVector, self.forceOfInfection(), label='Force of Infection')
         t = "Contact rate (%g, %g) - Infective period (%g, %g)\nInfective peak %g" % (
-            self.contactRate().min(), self.contactRate().max(),
-            self.averageInfectiousPeriod().min(), self.averageInfectiousPeriod().max(),
+            self.contactRate.min(), self.contactRate.max(),
+            self.averageInfectiousPeriod.min(),
+            self.averageInfectiousPeriod.max(),
             ts.infectives.max())
         plt.title(t)
         plt.legend()
